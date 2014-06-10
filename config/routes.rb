e2c21c42dc5ce215fe5a2d14ee08000259fb75c1
Liu Wei <wliu272@163.com>
2014-06-10 18:12:44 +0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Mockup::Application.routes.draw do
 
 
+  resources :welcomes
+
   resources :msg_show_log_histories
 
   resources :test_runs
@@ -28,7 +30,7 @@ Mockup::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605090518) do
+ActiveRecord::Schema.define(version: 20140610075728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,11 @@ ActiveRecord::Schema.define(version: 20140605090518) do
 
   create_table "test_runs", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "welcomes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
